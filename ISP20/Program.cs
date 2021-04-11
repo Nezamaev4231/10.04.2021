07ec1827db5db9fd51f667aac8aa3779a123e3db
unknown <you@example.c>
2021-04-11 22:34:25 +0300

    10.04.21

---
+++ ISP20/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISP20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1.15 б
+            Console.WriteLine("1.15 б");
+            Console.WriteLine("Введите число a");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите число b");
+            double b = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите число c");
+            double c = Convert.ToDouble(Console.ReadLine());
+            double a2 = a/(b * c);
+            Console.WriteLine("Ответ;" + a2);
+            Console.ReadKey();
+        }
+    }
+}
